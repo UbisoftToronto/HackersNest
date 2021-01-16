f8e4b518d8ea96ab978a2dc85da67d1cdb8dcad2
matt4323 <mattbenn4@hotmail.com>
2021-01-16 13:40:29 -0500

    Fixed move and background merge

--- UbiGame_Blank/Source/Game/GameBoard.cpp
+++ UbiGame_Blank/Source/Game/GameBoard.cpp
@@ -7,6 +7,21 @@
 
 using namespace Game;
 
+void GameBoard::CreatePlayer()
+{
+	m_player = new GameEngine::Entity();
+	GameEngine::GameEngineMain::GetInstance()->AddEntity(m_player);
+	
+	m_player->SetPos(sf::Vector2f(150.0f, 150.0f));  // <-- Move its initial position
+	m_player->SetSize(sf::Vector2f(50.0f, 50.0f)); // <-- Make the square bigger
+
+	GameEngine::RenderComponent* render = m_player->AddComponent<GameEngine::RenderComponent>(); // <-- Capturing the new component
+
+	render->SetFillColor(sf::Color::Red); // <-- Change the fill color to Red
+	m_player->AddComponent<Game::PlayerMovementComponent>();  // <-- Added the movement component to the player
+}
+
+
 GameBoard::GameBoard()
 {
 	CreatePlayer();
