fb5e4057967cb53c413a0feb820e398b51ec1660
Avery Chiu <averychiuu@gmail.com>
2021-01-16 12:42:00 -0500

    Changed sprite to rectangle

--- TheGhostBlewOutTheLight/Source/Game/GameBoard.cpp
+++ TheGhostBlewOutTheLight/Source/Game/GameBoard.cpp
@@ -7,7 +7,7 @@ using namespace Game;
 
 GameBoard::GameBoard()
 {
-    CreatePlayer(); // <-- Create Entity on Constructor
+    CreatePlayer();
 }
 
 void GameBoard::CreatePlayer()
@@ -16,7 +16,7 @@ void GameBoard::CreatePlayer()
 	GameEngine::GameEngineMain::GetInstance()->AddEntity(m_player);
 
 	m_player->SetPos(sf::Vector2f(50.0f, 50.0f));
-	m_player->SetSize(sf::Vector2f(50.0f, 50.0f));
+	m_player->SetSize(sf::Vector2f(10.0f, 40.0f));
 
 	//Render
 	GameEngine::RenderComponent* render = m_player->AddComponent<GameEngine::RenderComponent>();
