089ed4eafd2ce74c6b13b56d2f8f5cedf9e91cd6
matt4323 <mattbenn4@hotmail.com>
2021-01-16 14:29:27 -0500

    i messed up merging pls help

--- UbiGame_Blank/Source/Game/GameBoard.cpp
+++ UbiGame_Blank/Source/Game/GameBoard.cpp
@@ -15,6 +15,9 @@ void GameBoard::CreatePlayer()
 	m_player->SetPos(sf::Vector2f(150.0f, 150.0f));  // <-- Move its initial position
 	m_player->SetSize(sf::Vector2f(50.0f, 50.0f)); // <-- Make the square bigger
 
+	GameEngine::RenderComponent* render = m_player->AddComponent<GameEngine::RenderComponent>(); // <-- Capturing the new component
+
+	render->SetFillColor(sf::Color::Red); // <-- Change the fill color to Red
 	m_player->AddComponent<Game::PlayerMovementComponent>();  // <-- Added the movement component to the player
 }
 
@@ -22,15 +25,19 @@ void GameBoard::CreatePlayer()
 GameBoard::GameBoard()
 {
 	CreatePlayer();
-	CreateBackground();
+	//CreateBackground();
 }
 
 
-    GameEngine::SpriteRenderComponent* spriteRender = static_cast<GameEngine::SpriteRenderComponent*>
-    (m_player->AddComponent<GameEngine::SpriteRenderComponent>());
+GameBoard::~GameBoard()
+{
 
-    spriteRender->SetFillColor(sf::Color::Red);
-    spriteRender->SetTexture(GameEngine::eTexture::Pudge);
+}
+
+
+void GameBoard::Update()
+{	
+	
 }
 
 void GameBoard::CreateBackground() {
@@ -47,22 +54,3 @@ void GameBoard::CreateBackground() {
     render->SetTexture(GameEngine::eTexture::Background);
     render->SetFillColor(sf::Color::Transparent);
 }
-
-GameBoard::GameBoard()
-{
-	CreatePlayer();
-    //CreateBackground();
-}
-
-
-GameBoard::~GameBoard()
-{
-
-}
-
-
-void GameBoard::Update()
-{	
-	
-}
-
