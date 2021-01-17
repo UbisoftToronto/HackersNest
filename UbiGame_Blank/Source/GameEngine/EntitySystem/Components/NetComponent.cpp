1e6a6363b4350580400ecf47b84d77cde0f47f1d
Nick <n.faria373@gmail.com>
2021-01-17 01:48:10 -0500

    Finished net component

--- UbiGame_Blank/Source/GameEngine/EntitySystem/Components/NetComponent.cpp
+++ UbiGame_Blank/Source/GameEngine/EntitySystem/Components/NetComponent.cpp
@@ -18,26 +18,16 @@ void NetComponent::Update()
 
     sf::Vector2f displacement{ 0.0f,0.0f };
 
-    float inputAmount = 125.0f;
+    float inputAmount = 145.0f;
 
     sf::Vector2f windowPos{ static_cast<float>(GetEntity()->window->getPosition().x), static_cast<float>(GetEntity()->window->getPosition().y) };
 
     sf::Vector2f destination{destination_x, destination_y};
     sf::Vector2f pos_diff = destination - GetEntity()->GetPos() - windowPos;
 
-    float vector_length = sqrt(pos_diff.x * pos_diff.x + pos_diff.y * pos_diff.y);
+    float vector_length = sqrt(pos_diff.x * pos_diff.x + pos_diff.y * pos_diff.y);=
 
-    GetEntity()->SetPos(GetEntity()->GetPos() + displacement);
-
-    sf::Vector2f unit{0.f,1.f};
-
-    float dot = pos_diff.x*unit.x + pos_diff.y*unit.y;
-    float det = pos_diff.x*unit.y - pos_diff.y*unit.x;
-    float angle = 180*atan(dot/det)/3.14;
-    if(pos_diff.x < 0){
-        angle += 180;
-    }
-    GetEntity()->SetRotation(angle);
+    GetEntity()->rotate(dt*5);
 
     if (liveTime > 0) {
         displacement.x += inputAmount * (pos_diff.x / vector_length) * dt;
@@ -46,6 +36,8 @@ void NetComponent::Update()
     } else {
         GameEngine::GameEngineMain::GetInstance()->RemoveEntity(GetEntity());
     }
+
+    GetEntity()->SetPos(GetEntity()->GetPos() + displacement);
 
 }  
 
